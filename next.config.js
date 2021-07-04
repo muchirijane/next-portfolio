48aae75a0c91bdcd19d62205f881dba4d1d33f29
muchirijane <muchirijane@outlook.com>
2021-07-04 09:06:20 +0300

    fixing a webpack bug from babel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-	// webpack: (config, { isServer }) => {
-	// 	if (!isServer) {
-	// 		config.resolve.fallback.fs = false;
-	// 	}
-	// 	return config;
-	// },
 	images: {
 		domains: [ 'images.ctfassets.net' ]
 	}
