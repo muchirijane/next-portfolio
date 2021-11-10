1954794de77d42c6efc6935acfea69169003196a
muchirijane <muchirijane@outlook.com>
2021-11-10 22:39:59 +0300

    adding some portfolio projects

--- next.config.js
+++ next.config.js
@@ -1,26 +1,19 @@
-// const withPWA = require('next-pwa');
-// const runtimeCaching = require('next-pwa/cache');
-// module.exports = withPWA({
-// 	pwa: {
-// 		dest: 'public',
-// 		runtimeCaching
-// 	},
-// 	images: {
-// 		domains: [ 'images.ctfassets.net' ]
-// 	}
-// });
-
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')
 
 module.exports = withPWA({
-	pwa: {
-		disable: process.env.NODE_ENV === 'development',
-		register: true,
-		scope: '/app',
-		sw: 'service-worker.js'
-		//...
-	},
-	images: {
-		domains: [ 'images.ctfassets.net' ]
-	}
-});
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    scope: '/app',
+    sw: 'service-worker.js',
+    //...
+  },
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+  env: {
+    // Will be available on both server and client
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+  },
+})
--- pages/blog.js
+++ pages/blog.js
@@ -1,37 +1,44 @@
-import { createClient } from 'contentful';
-import Head from 'next/head';
-import Layout from '../components/layout/Layout';
-import BlogCard from '../components/template/BlogCard';
-import { BlogWrapper, BlogContainer } from '../styles/Blog.styles';
+import { createClient } from 'contentful'
+import Head from 'next/head'
+import Layout from '../components/layout/Layout'
+import BlogCard from '../components/template/BlogCard'
+import { BlogWrapper, BlogContainer } from '../styles/Blog.styles'
 export async function getStaticProps() {
-	const client = createClient({
-		space: process.env.CONTENTFUL_SPACE_ID,
-		accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-	});
+  const space = process.env.CONTENTFUL_SPACE_ID
+  const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN
 
-	const res = await client.getEntries({ content_type: 'portfolio' });
-	return {
-		props: { blogs: res.items },
-		revalidate: 2
-	};
+  const client = createClient({
+    space: space,
+    accessToken: accessToken,
+  })
+
+  const res = await client.getEntries({ content_type: 'portfolio' })
+  return {
+    props: { blogs: res.items },
+    revalidate: 2,
+  }
 }
 
 export default function Blog({ blogs }) {
-	console.log(blogs);
-	return (
-		<Layout>
-			<Head>
-				<title>My Blog</title>
-			</Head>
-			<BlogWrapper>
-				<h1>
-					My Blog{' '}
-					<span role="img" alt="a writing emoji">
-						✍🏼
-					</span>{' '}
-				</h1>
-				<BlogContainer>{blogs.map((blog) => <BlogCard key={blog.sys.id} blog={blog} />)}</BlogContainer>
-			</BlogWrapper>
-		</Layout>
-	);
+  console.log(blogs)
+  return (
+    <Layout>
+      <Head>
+        <title>My Blog</title>
+      </Head>
+      <BlogWrapper>
+        <h1>
+          My Blog{' '}
+          <span role="img" alt="a writing emoji">
+            ✍🏼
+          </span>{' '}
+        </h1>
+        <BlogContainer>
+          {blogs.map((blog) => (
+            <BlogCard key={blog.sys.id} blog={blog} />
+          ))}
+        </BlogContainer>
+      </BlogWrapper>
+    </Layout>
+  )
 }
