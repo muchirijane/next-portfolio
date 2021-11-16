eefeddd9aa6037737801e1fbfb5c1bcdec92ee8d
muchirijane <muchirijane@outlook.com>
2021-11-16 08:06:00 +0300

    formatting the blog styles

--- components/template/BlogCard.styles.js
+++ components/template/BlogCard.styles.js
@@ -5,19 +5,13 @@ export const BlogContent = styled.div`
   width: 36rem;
   transition: all 0.2s cubic-bezier(0.88, 0.17, 0.05, 1);
   background: #242424;
-  border-radius: 2 rem;
-  border: 1 px solid #444;
+  border-radius: 2rem;
+  border: 1px solid #444;
   overflow: hidden;
 
   &:hover {
     transition: all 0.2s cubic-bezier(0.88, 0.17, 0.05, 1);
     box-shadow: var(--medium-shadow);
-
-    /* span {
-      color: var(--colour-white);
-      padding: 0.5rem;
-      border-radius: 0.4rem;
-    } */
 
     img {
       transition: all 0.2s cubic-bezier(0.88, 0.17, 0.05, 1);
@@ -44,38 +38,20 @@ export const BlogItems = styled.div`
 
 export const BlogTagWrapper = styled.div`
   margin-top: 2rem;
+  display: flex;
+  align-items: center;
   span {
-    background: linear-gradient(
-      90deg,
-      rgba(255, 118, 20, 1) 0%,
-      rgba(255, 84, 17, 1) 100%
-    );
+    background: #444;
+
     color: var(--colour-white);
     padding: 0.5rem;
     border-radius: 0.4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     &:not(:last-child) {
       padding-right: 2rem;
-    }
-    &:nth-child(4n + 1) {
-      background: linear-gradient(
-        90deg,
-        rgb(129, 110, 167) 0%,
-        rgba(155, 0, 250, 1) 100%
-      );
-    }
-    &:nth-child(4n + 2) {
-      background: linear-gradient(
-        90deg,
-        rgba(255, 12, 241, 1) 0%,
-        rgba(250, 0, 135, 1) 100%
-      );
-    }
-    &:nth-child(4n + 3) {
-      background: linear-gradient(
-        90deg,
-        rgba(20, 159, 255, 1) 0%,
-        rgba(17, 122, 255, 1) 100%
-      );
+      margin-right: 1rem;
     }
   }
 `
